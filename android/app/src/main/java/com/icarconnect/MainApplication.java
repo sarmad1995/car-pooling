b3da939786a36269f3ba4e169ef40cc28cdbc0f3
Sarmad Aijaz <sarmad@Sarmads-MacBook-Air.local>
2018-07-14 22:56:49 +0530

    modified location

--- android/app/src/main/java/com/icarconnect/MainApplication.java
+++ android/app/src/main/java/com/icarconnect/MainApplication.java
@@ -3,6 +3,7 @@ package com.icarconnect;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.showlocationservicesdialogbox.LocationServicesDialogBoxPackage;
 import com.marianhello.bgloc.react.BackgroundGeolocationPackage;
 import com.mustansirzia.fused.FusedLocationPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -31,6 +32,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LocationServicesDialogBoxPackage(),
             new BackgroundGeolocationPackage(),
             new FusedLocationPackage(),
             new MapsPackage(),
